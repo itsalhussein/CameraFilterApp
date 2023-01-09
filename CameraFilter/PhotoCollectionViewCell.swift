41dcabf2f7e591061e68bae2acbb8de9b9202dea
Hussein <hussein.elsherief@gmail.com>
2023-01-09 17:29:54 +0200

    - Added the main screen and photos collection view screen

---
+++ CameraFilter/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CameraFilter
+//
+//  Created by Al-Hussein Anwar on 09/01/2023.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
--- CameraFilter/ViewController.swift
+++ CameraFilter/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var applyFilterButton: UIButton!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard let navC = segue.destination as? UINavigationController,
+              let photosVC = navC.viewControllers.first as? PhotosCollectionViewController else { return }
+        
+        photosVC.selectedPhoto.subscribe(onNext: { [weak self] photo in
+            self?.photoImageView.image = photo
+        }).disposed(by: disposeBag)
+        
+        
+    }
+    
+    
+    
 }
-
